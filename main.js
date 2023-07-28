0212081f34204296cde0dea666028105e2af75db
Luis Carlos Bravo <luiscbravo94@gmail.com>
2023-07-28 22:57:38 +0100

    agregamos el menu mobile, y agregamos otra media query

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const navbar_email = document.querySelector('.navbar-email')
 const desktop_orders = document.querySelector('.desktop-menu')
+const menu_hamburguesa_mobile = document.querySelector('.menu')
+const menu_mobile = document.querySelector('.mobile-menu')
 
 navbar_email.addEventListener('click',display_order)
-document.addEventListener('click',inactive)
+menu_hamburguesa_mobile.addEventListener('click',display_menu)
+
 function display_order() {
     desktop_orders.classList.toggle('inactive')
+}
+
+function display_menu(){
+    menu_mobile.classList.toggle('inactive')
 }
 
